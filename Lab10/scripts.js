73582c4dbc7c4d9ca98bce692d6415f33c8442cb
cv22am <cv22am@brocku.ca>
2023-11-13 15:34:32 -0500

    Lab 10 completed

---
+++ Lab10/scripts.js
@@ -0,0 +1,33 @@
+function outputAdd() {
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  x = parseFloat(x);
+  y = parseFloat(y);
+  var    results = x + y;
+  document.getElementById("outputText").innerHTML=results;
+}
+
+function outputSub() {
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  x = parseFloat(x);
+  y = parseFloat(y);
+  var    results = x - y;
+  document.getElementById("outputText").innerHTML=results;
+}
+function outputMult() {
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  x = parseFloat(x);
+  y = parseFloat(y);
+  var    results = x * y;
+  document.getElementById("outputText").innerHTML=results;
+}
+function outputDivide() {
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  x = parseFloat(x);
+  y = parseFloat(y);
+  var    results = x / y;
+  document.getElementById("outputText").innerHTML=results;
+}
