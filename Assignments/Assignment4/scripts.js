91130b7fc985985e038a21a1c1bc8c04d8bd2caa
cv22am <cv22am@brocku.ca>
2023-11-16 01:53:34 -0500

    Added video, scaled main1 properly, added nav, added images, added hover and scrolling text

---
+++ Assignments/Assignment4/scripts.js
@@ -0,0 +1,11 @@
+
+
+function changeVisOff() {
+  document.getElementById("video1").style.visibility = "hidden";
+
+}
+
+function changeVisOn() {
+  document.getElementById("video1").style.visibility = "visible";
+
+}
